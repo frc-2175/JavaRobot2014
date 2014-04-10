30858eb0df5eaa960da5488e4895aa7dff68f0e7
Aren Siekmeier <compwiztobe@gmail.com>
2014-04-09 23:29:04 -0500

    changed some things around, made logic more concise, changed branching flow so that driving always happens on new forward and then it saves for next loop

--- src/org/usfirst/frc2175/Robot2014/subsystems/SmoothDrive.java
+++ src/org/usfirst/frc2175/Robot2014/subsystems/SmoothDrive.java
@@ -3,31 +3,33 @@ package org.usfirst.frc2175.Robot2014.subsystems;
 import org.usfirst.frc2175.Robot2014.Robot;
 
 public class SmoothDrive {
-    private double previousOutput = 0;
+    private double previousForward = 0;
+    private double newForward = 0;
     private static final double MAX_CHANGE = 0.1;
 
     // Subsystem stuff
 
     void execute() {
-        if (previousOutput * Robot.oi.getForwardSpeed() <= 0 && Math.abs(Robot.oi.getForwardSpeed() - previousOutput) > MAX_CHANGE) {
+        if (previousForward * Robot.oi.getForwardSpeed() <= 0 && Math.abs(Robot.oi.getForwardSpeed() - previousOutput) > MAX_CHANGE) {
             // The signs are different (or one value is 0) AND we are changing rapidly enough to merit smoothing
 
             int sign;
-            if (Robot.oi.getForwardSpeed() < 0) {
+            if (Robot.oi.getForwardSpeed() < 0 || previousForward > 0) {
                 sign = -1;
-            } else if (Robot.oi.getForwardSpeed() > 0) {
+            } else if (Robot.oi.getForwardSpeed() > 0 || previousForward < 0) {
                 sign = 1;
-            } else {
-                sign = (int)(-(previousOutput / Math.abs(previousOutput))); // If we try to go to 0, we have to use the opposite sign of what we were just outputting
+            } else { // just in case something crazy happened...
+                sign = 0;//(int)(-(previousOutput / Math.abs(previousOutput))); // If we try to go to 0, we have to use the opposite sign of what we were just outputting
             }
-            double newOutput = previousOutput + (MAX_CHANGE * sign);
-            // Drive here with newOutput
-            previousOutput = newOutput;
+            newForward = previousForward + (MAX_CHANGE * sign);
         } else {
-            // Otherwise, don't bother to smooth!
-            
-            // Drive here Robot.oi.getForwardSpeed();
-            previousOutput = Robot.oi.getForwardSpeed();
+            // Otherwise, don't bother to drive smooth!
+            newForward = Robot.oi.getForwardSpeed();
         }
+
+	// drive with newForward value
+
+	// save output for next loop
+	previousForward = newForward;
     }
 }
