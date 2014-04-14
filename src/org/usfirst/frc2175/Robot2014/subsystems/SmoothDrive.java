5329c9dd1f9572502a1a8e06d0ae642fdff322ec
Aren Siekmeier <compwiztobe@gmail.com>
2014-04-14 16:28:44 -0500

    cleaned up some stuff, added time check to scale max change appropriately
    put in appropriate value for max change based on math
    
    however, we must TEST this value, and make sure to tune it, before changing
    any other conditions or parameters.  It is almost guaranteed not to work the first time, but that's ok, because we can survive a little bit of work

--- src/org/usfirst/frc2175/Robot2014/subsystems/SmoothDrive.java
+++ src/org/usfirst/frc2175/Robot2014/subsystems/SmoothDrive.java
@@ -3,39 +3,40 @@ package org.usfirst.frc2175.Robot2014.subsystems;
 import org.usfirst.frc2175.Robot2014.Robot;
 
 public class SmoothDrive {
-    private double previousForward = 0;
-    private double newForward = 0;
-    private static final double MAX_CHANGE = 0.1;
-    private double prevTime = 0; // initialize this...
-
+    private double prevForward, newForward;
+    private double prevTime, newTime;
+    private static final double MAX_CHANGE = 4/3; // see math...
+    
     // Subsystem stuff
+    
+    void initialize() {
+          prevTime = newTime = edu.wpi.first.wpilibj.Timer.getFPGATimestamp();
+          prevForward = newForward = 0;
+    }
 
     void execute() {
         // compute time since last iteration.... newTime - prevTime
-	// adjust MAX_CHANGE accordingly to limit accel rather than dv
-
-        if (//previousForward * Robot.oi.getForwardSpeed() <= 0 && 
-                Math.abs(Robot.oi.getForwardSpeed() - previousForward) > MAX_CHANGE) {// && 
-                //Robot.drivetrain.GetShiftState()) {
-            // The signs are different (or one value is 0) AND we are changing rapidly enough to merit smoothing AND we are in high gear
-            // We'll probably need to do this even if the product is positive (same signs)
-            // consider accelerating from zero: the first step sees prev=0, new=1;
-            // the next one sees prev=.1, new=1, so it immediately jumps to 1, giving up on any accel control...
-
-            int sign;
-            if (Robot.oi.getForwardSpeed() > previousForward)
-		sign = 1;
-            else
-		sign = -1;
-            newForward = previousForward + (MAX_CHANGE * sign);
+	newTime = edu.wpi.first.wpilibj.Timer.getFPGATimestamp();
+        double dt = newTime - prevTime;
+        
+        // a few extra conditions in here to test -- tune MAX_CHANGE first!!!
+        if (
+                //previousForward * Robot.oi.getForwardSpeed() <= 0 && 
+                Math.abs(Robot.oi.getForwardSpeed() - prevForward)*dt > MAX_CHANGE// && 
+                //Robot.drivetrain.GetShiftState()
+        ) {
+            int sign = (Robot.oi.getForwardSpeed() > prevForward) ? 1 : -1;
+            newForward = prevForward + sign * MAX_CHANGE;
         } else {
             // Otherwise, don't bother to drive smooth!
             newForward = Robot.oi.getForwardSpeed();
         }
 
 	// drive with newForward value and joystick turning value
-
+        
+        
 	// save output for next loop
-	previousForward = newForward;
+	prevForward = newForward;
+        prevTime = newTime;
     }
 }
