93865c5d786526810b261d14baa8323563487c3f
Aren Siekmeier <compwiztobe@gmail.com>
2014-04-15 09:44:11 -0500

    Prettifying, various cleanup.
    cleaned up timing code, variable names, etc.
    
    also note, extra conditions have been removed until extensive shows a need for them
    
    TUNE the input constant!!!  Start with it small, then gradually increase, making it as large as possible without ever allowing wheels to leave the ground.  Since this is how we should always be driving anyway, we won't need any other checks.

--- src/org/usfirst/frc2175/Robot2014/Ramp.java
+++ src/org/usfirst/frc2175/Robot2014/Ramp.java
@@ -4,38 +4,38 @@ import org.usfirst.frc2175.Robot2014.Robot;
 
 public class Ramp {
     private double output;
-    private double prevTime, newTime;
-    private double MAX_CHANGE;
+    private double time;
+    private double max_delta;
        
     public Ramp(double max) {
-          MAX_CHANGE = max;
+        max_delta = max;
     }
     
     public Ramp() { this(4/3); }
     
     public void init(double input) {
-        prevTime = newTime = edu.wpi.first.wpilibj.Timer.getFPGATimestamp();
+        time = edu.wpi.first.wpilibj.Timer.getFPGATimestamp();
         output = input;
     }
     
     public void init() { this.init(0); }
 
     public double rampInput(double input) {
-        // compute time since last iteration.... newTime - prevTime
-	newTime = edu.wpi.first.wpilibj.Timer.getFPGATimestamp();
-        double dt = newTime - prevTime;
-        
-        // a few extra conditions in here to test -- tune MAX_CHANGE first!!!
-        if (input > output + MAX_CHANGE*dt) {
-                output += MAX_CHANGE*dt;
-        } else if (input < output - MAX_CHANGE*dt) {
-                output -= MAX_CHANGE*dt;
+        double newTime = edu.wpi.first.wpilibj.Timer.getFPGATimestamp();
+        double dt = newTime - time;
+
+        // check both bounds and coerce
+        // TUNE max_delta!!!!
+        if (input > output + max_delta*dt) {
+            output += max_delta*dt;
+        } else if (input < output - max_delta*dt) {
+            output -= max_delta*dt;
         } else {
             output = input;
         }
 
-	prevTime = newTime;
-        
+        time = newTime;
+
         return output;
     }
 }
